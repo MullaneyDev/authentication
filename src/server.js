c934b5412cf39db2b8b69f83311298e20e23c1ed
MullaneyDev <matt.mullaney@live.co.uk>
2023-10-23 10:53:13 +0100

    example of middleware

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 
+const router = require("./middlewareExample")
+
 const port = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
+app.use("/example", router)
 
 app.get("/health", (req, res) => {
   req.json(200).json({ message: "API is healthy" });
 });
 
 app.listen(port, () => {
-  console.log("app is listening");
+  console.log("App is listening");
 });
